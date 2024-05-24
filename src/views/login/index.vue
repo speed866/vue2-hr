eb7494d257207620a83cc5969cb9256ba8614973
Jesse Wang <jiojiodibulido@gmail.com>
2024-05-24 09:30:35 +0800

    完成登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,18 +5,18 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="请输入手机号" />
+        <el-form ref="form" :rules="loginRules" :model="loginForm">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" placeholder="请输入密码" show-password />
+          </el-form-item>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree">用户平台使用协议</el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="请输入密码" />
-          </el-form-item>
-          <el-form-item>
-            <el-checkbox>用户平台使用协议</el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" style="width: 350px">登录</el-button>
+            <el-button type="primary" style="width: 350px" @click="login">登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -25,7 +25,47 @@
 </template>
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '18295250815',
+        password: '111111',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码长度应为6-16位之间', trigger: 'blur' }
+        ],
+        isAgree: [
+          { validator: (rule, value, callback) => {
+            value ? callback() : callback(new Error('请勾选使用协议'))
+          }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$message({
+            message: '校验通过',
+            type: 'success'
+          })
+        } else {
+          this.$message.error('校验失败')
+          return false
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
